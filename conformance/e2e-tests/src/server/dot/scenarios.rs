9005cb7da2f9d9820cbdf449cb1cb9ac5fc7875f
Daniel McCarney <daniel@binaryparadox.net>
2025-09-25 15:54:09 -0400

    conformance: quick-n-dirty e2e hickory opportunistic enc test
    Just smoke-testing this branch with a quick hacky test.

--- conformance/e2e-tests/src/server/dot/scenarios.rs
+++ conformance/e2e-tests/src/server/dot/scenarios.rs
@@ -124,6 +124,110 @@ fn pdns_opportunistic_dot_success() -> Result<(), Error> {
     Ok(())
 }
 
+#[test]
+fn hickory_opportunistic_dot_success() -> Result<(), Error> {
+    let network = &Network::new()?;
+    let leaf_ns = NameServer::builder(
+        Implementation::hickory(),
+        FQDN::TEST_DOMAIN,
+        network.clone(),
+    )
+    .pki(Pki::new()?.into())
+    .build()?;
+
+    let Graph {
+        root: root_info,
+        nameservers,
+        ..
+    } = Graph::build(leaf_ns, Sign::No)?;
+
+    // TODO(XXX): Presently auth. hickory servers do not send referrals properly,
+    //   and so only the root NS sees queries. Once #2810 is fixed we should switch
+    //   this test to ensure the other 2 auth. nameservers see expected traffic.
+    let root_ns = nameservers.last().unwrap();
+
+    // Capture nameserver traffic on UDP 53 and TCP 853 to get both plaintext DNS
+    // and DoT requests/responses.
+    let mut tshark = Tshark::builder()
+        .filters(vec![
+            ProtocolFilter::default(),
+            ProtocolFilter::default()
+                .protocol(Protocol::Tcp)
+                .port(DOT_PORT),
+        ])
+        .ssl_keylog_file("/tmp/sslkeys.log") // See hickory.Dockerfile
+        .build(root_ns.container())?;
+
+    // Create a HickoryDNS resolver configured for DoT probing.
+    let resolver = Resolver::new(network, root_info)
+        .custom_config(HICKORY_RECURSOR_PROBE_CONFIG.to_string())
+        .start_with_subject(&Implementation::hickory())?;
+
+    // Make a recursive query for the root FQDN A record using the resolver instance.
+    let client = Client::new(network)?;
+    let query_settings = *DigSettings::default().recurse();
+    let resolver_ip = resolver.ipv4_addr();
+    let resp = client.dig(query_settings, resolver_ip, RecordType::A, &FQDN::ROOT)?;
+    assert!(resp.status.is_noerror());
+
+    // Wait until we've captured at least one incoming DoT probe at the nameserver.
+    tshark.wait_until(
+        |captures| {
+            captures.iter().any(|c| {
+                matches!(c.direction, Direction::Incoming { .. }) && c.dst_port == DOT_PORT
+            })
+        },
+        Duration::from_secs(10),
+    )?;
+
+    // Now, make a final query for yet another record type. The recursor should use DoT to satisfy
+    // the request based on its prior probing success.
+    let resp = client.dig(query_settings, resolver_ip, RecordType::MX, &FQDN::ROOT)?;
+    assert!(resp.status.is_noerror());
+
+    // Wait until we've seen an incoming DoT query for the correct record name and type.
+    // This is the query from the resolver that was upgraded to DoT opportunistically.
+    tshark.wait_until(
+        |captures| {
+            captures.iter().any(|c| {
+                matches!(c.direction, Direction::Incoming { .. })
+                    && c.dst_port == DOT_PORT
+                    && query_name_and_type(c) == ("<Root>", record_types::MX)
+            })
+        },
+        Duration::from_secs(10),
+    )?;
+
+    // Partition the captured incoming queries based on their dest port.
+    let (dot_queries, udp_queries) = tshark
+        .terminate()?
+        .into_iter()
+        .filter(|c| matches!(c.direction, Direction::Incoming { .. }))
+        .partition::<Vec<_>, _>(|m| m.dst_port == DOT_PORT);
+
+    // We should have received 2 UDP queries from the recursive resolution:
+    //  * First: NS query for root (internal to find nameservers)
+    //  * Second: A query for root (the actual requested record)
+    assert_eq!(udp_queries.len(), 2);
+    assert_eq!(
+        query_name_and_type(&udp_queries[0]),
+        ("<Root>", record_types::NS)
+    );
+    assert_eq!(
+        query_name_and_type(&udp_queries[1]),
+        ("<Root>", record_types::A)
+    );
+
+    // We should have received at least 2 DoT queries:
+    //  * At least one probe lookup from the recursor opportunistically checking us out after our
+    //    second query.
+    //  * Our second and final query, a lookup for the root's MX record. We verified
+    //    its presence in the last `wait_until()`.
+    assert!(dot_queries.len() >= 2);
+
+    Ok(())
+}
+
 // Perform serde_json::Value acrobatics to extract the qry name and rr type from capture's first query.
 fn query_name_and_type(c: &Capture) -> (&str, u16) {
     let message_map = c.message.as_value().as_object().unwrap();
@@ -169,6 +273,23 @@ logging:
   quiet: false
 "#;
 
+static HICKORY_RECURSOR_PROBE_CONFIG: &str = r#"
+user = "nobody"
+group = "nogroup"
+
+[[zones]]
+zone = "."
+zone_type = "External"
+
+[zones.stores]
+type = "recursor"
+roots = "/etc/root.hints"
+dnssec_policy = "ValidationDisabled"
+allow_server = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+[zones.stores.opportunistic_encryption]
+enabled = "true"
+"#;
+
 const DOT_PORT: u16 = 853;
 
 /// An internal module for record type constants.
@@ -177,6 +298,7 @@ const DOT_PORT: u16 = 853;
 /// on hickory-proto just for matching to expected.
 mod record_types {
     pub(super) const A: u16 = 1;
+    pub(super) const NS: u16 = 2;
     pub(super) const AAAA: u16 = 28;
     pub(super) const MX: u16 = 15;
 }
